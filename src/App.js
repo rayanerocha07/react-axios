e960b29215598b4a33a5011d3582530988cd6116
Rayane Rocha <rayane.rocha07@gmail.com>
2022-01-04 12:21:06 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+export default function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    api
+      .get('/users/gustavoguanabara')
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error('ops! ocorreu um erro' + err);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <p>Usuário: {user?.login}</p>
+      <p>Biografia: {user?.bio}</p>
+    </div>
+  );
+}
